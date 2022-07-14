1ba36bc95a6c1f0c5ee9745d92514f14d7b3e233
erica7monteiro@gmail.com <erikaisa7dev@gmail.com>
2022-07-14 14:44:03 -0300

    testing at message broker

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import client, {Connection, Channel, ConsumeMessage } from 'amqplib'
+
+// Function to send some messages before consuming the queue
+const sendMessages = (channel: Channel) => {
+  for (let i = 0; i < 10; i++) {
+    channel.sendToQueue('myQueue', Buffer.from(`message ${i}`))
+  }
+}
+
+// consumer for the queue
+const consumer = (channel: Channel) => (msg: ConsumeMessage | null): void => {
+  if (msg) {
+    // Display the received message
+    console.log(msg.content.toString())
+    // Acknowledge the message
+    channel.ack(msg)
+  }
+}
+
+
+// Create a channel
+
+const start = async () => {
+  const connection: Connection = await client.connect('amqp://username:password@localhost:5672')
+  // Create a channel
+  const channel: Channel = await connection.createChannel()
+  // Makes the queue available to the client
+  await channel.assertQueue('myQueue')
+  // Send some messages to the queue
+  sendMessages(channel)
+  // Start the consumer
+  await channel.consume('myQueue', consumer(channel))
+}
+
+start()
